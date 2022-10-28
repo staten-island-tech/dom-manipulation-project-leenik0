bde2cd766e550454d9cc3388b4beb7150c0f2b87
leenik0 <nicoleh.vafiadis@gmail.com>
2022-10-28 09:29:56 -0400

    jjjj

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMselectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  point: document.querySelectorAll(".point"),
+  pointTwo: document.getElementById("point2"),
+};
+console.log(DOMselectors.pointTwo);
